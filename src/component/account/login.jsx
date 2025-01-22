58642f248e46d4104121a6a48737c5af0fc77da6
Anshul <ansh26748ar@gmail.com>
2025-01-22 18:51:09 +0530

    Login page/sigup page

--- src/component/account/login.jsx
+++ src/component/account/login.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, TextField, styled } from '@mui/material';
+import { Box, Button, TextField, styled, Typography } from '@mui/material';
 
 const Containers_image = styled('img')({
     width: '100%',
@@ -8,23 +8,53 @@ const Containers_image = styled('img')({
 });
 
 const Component = styled(Box)`
-    width: 400px;
-    margin: auto;
-    box-shadow: 10px 15px 5px 5px rgb(0 0 0/ 0.6);
+    width: 350px;
+    // margin: auto;
+    box-shadow: 7px 7px rgb(0 0 0/ 0.6);
     position: absolute; // Absolute positioning for overlay
     top: 50%; // Center vertically
     left: 50%; // Center horizontally
     transform: translate(-50%, -50%); // Adjust position for perfect centering
-    background: rgba(255, 255, 255, 0.9); // Add a semi-transparent background
-    padding: 20px; // Add some padding
-    border-radius: 8px; // Add rounded corners
+    background: rgba(255, 255, 255, 0.6); // Add a semi-transparent background
+    padding-top: 20px ; 
+    border-radius: 8px; 
 `;
 
+const Wrapper = styled(Box)`
+    padding-left: 30px;
+    padding-right: 30px;
+    padding-bottom: 25px;
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    & > div, & > button, & > p {
+        margin-top: 13px;
+    }
+`;
 const Image = styled('img')({
-    width: 390,
-    height: 200,
-    marginBottom: 16, // Space below the image
+    width: 300,
+    height: 170,
+    margin: 'auto',
+    display: 'flex',
+
 });
+
+const LoginButton = styled(Button)`
+    text-transform: none;
+    border-radius: 2px;
+    box-shadow: 3px 3px rgb(0 0 0/50%);
+`
+
+const SignupButton = styled(Button)`
+    text-transform: none;
+    background: rgb(255 255 255);
+    border-radius: 2px;
+    box-shadow: 3px 3px rgb(0 0 0/50%);
+`
+const Text = styled(Typography)`
+    color: rgb(0 0 255);
+    
+`
 
 const Login = () => {
     const conatiner =
@@ -36,11 +66,25 @@ const Login = () => {
         <Box sx={{ position: 'relative', height: '100vh' }}>
             <Containers_image src={conatiner} alt="container image" />
             <Component>
-                <Image src={imageurl} alt="LOGIN image" />
-                <TextField label="Email" variant="standard"/>
-                <TextField label="Password" variant="standard"/>
-                <Button variant="contained">Login</Button>
-                <Button>Create an Account</Button>
+                <Box>
+                    <Image src={imageurl} alt="LOGIN image" />
+                    <Wrapper>
+                        <TextField label="Email" variant="standard"/>
+                        <TextField label="Password" variant="standard"/>
+                        <LoginButton variant="contained">Login</LoginButton>
+                        <Text style={{textAlign: 'center'}} >OR</Text>
+                        <SignupButton>Create an Account</SignupButton>
+                    </Wrapper>   
+                    {/* <Wrapper>
+                        <TextField label="Name" variant='standard' ></TextField>
+                        <TextField label="Email" variant="standard" />
+                        <TextField label="Password" variant="standard" />
+                        <SignupButton>Signup</SignupButton>
+                        <Text style={{ textAlign: 'center' }} >OR</Text>
+                        <LoginButton variant="contained">Already have an Account</LoginButton>
+                    </Wrapper> */}
+
+                </Box>
             </Component>
         </Box>
     );
